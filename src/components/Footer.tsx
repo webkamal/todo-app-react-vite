7a6bf1f698cb583f37e334ede3076cc4b72174e8
Webkamal <ozzykamal@gmail.com>
2025-04-05 23:11:56 +1100

    copyright to reactking

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="w-[972px] flex justify-between items-center text-[11px] opacity-30 mt-[12px]">
-      <small className="text-[11px]">&copy; 2050. Copyright by ByteGrad.</small>
+      <small className="text-[11px]">
+        &copy; 2050. Copyright by ReactKing.
+      </small>
       <p>
         Version <b>1.5</b>
       </p>
