52876500fd8e442b605ccd541f2276e84a42341c
Kamal Khanal <ozzykamal@gmail.com>
2025-04-07 13:47:58 +1000

    Update TodoList.tsx
    updating the whole code as per Ai suggest

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,42 +1,32 @@
-import DeleteButton from "./DeleteButton";
-import { useTodosContext } from "../lib/hooks";
+import { useContext } from 'react';
+import { TodosContext } from '../contexts/TodosContextProvider';
+import { Todo } from '../types';
+import DeleteButton from './DeleteButton';
 
 export default function TodoList() {
-  const { todos, toggleTodo, isLoading } = useTodosContext();
+  const { todos, toggleTodo } = useContext(TodosContext)!;
 
   return (
-    <ul className="col-[1/2] row-[2/3] bg-[#fff] [scrollbar-width:thin] relative">
-      {isLoading && (
-        <li className="h-full flex justify-center items-center font-semibold">
-          Loading todos...
-        </li>
-      )}
-
+    <div className="todo-list">
       {todos.length === 0 ? (
-        <li className="h-full flex justify-center items-center font-semibold">
-          Start by adding a todo
-        </li>
-      ) : null}
-
-      {todos.map((todo) => {
-        return (
-          <li
-            key={todo.id}
-            className={`flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-b-[rgba(0,0,0,0.08)]`}
-            onClick={() => {
-              toggleTodo(todo.id);
-            }}
-          >
-            <span
-              className={`${todo.completed ? "line-through text-[#ccc]" : ""}`}
-            >
-              {todo.content}
-            </span>
-
-            <DeleteButton id={todo.id} />
-          </li>
-        );
-      })}
-    </ul>
+        <p>No todos yet!</p>
+      ) : (
+        <ul>
+          {todos.map((todo: Todo) => (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => toggleTodo(todo.id)}
+              />
+              <span className={todo.completed ? 'completed' : ''}>
+                {todo.content}
+              </span>
+              <DeleteButton id={todo.id} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
